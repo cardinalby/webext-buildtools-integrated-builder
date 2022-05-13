0d89af12f6e6ecaa8d6d661ded6bb7c2957a9160
Cardinal <cardinalby@gmail.com>
2022-05-13 15:29:52 +0300

    update deps, fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { IntegratedBuilder } from './builder';
 
 export default IntegratedBuilder;
 
-export * from '../declarations/options';
-export * from '../declarations/buildRunnerOptions';
-export * from './buildResult';
-export * from './buildRunner';
+export {IIntegratedOptions} from '../declarations/options';
+export {IBuildRunnerOptions} from '../declarations/buildRunnerOptions';
+export {IntegratedBuildResult} from './buildResult';
+export {startBuild} from './buildRunner';
